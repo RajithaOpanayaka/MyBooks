7c934c45e4de13e1aa7d044689d85c7e155c95d2
Rajitha Opanayaka <rajithaopanayaka.ro@gmail.com>
2020-02-02 16:59:24 +0530

    add auth

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express     =require("express"),
 	  Comment   =require("./models/comment"),
 	  User      =require("./models/user"),
 	mongoose    =require("mongoose"),
+	passport    =require("passport"),
+  LocalStrategy =require("passport-local"),
   bodyParser    =require("body-parser"),
   booksRoutes   =require("./routes/books"),
   commentRoutes =require("./routes/comments");
@@ -22,6 +24,22 @@ app.listen(3000,function(){
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.set("view engine","ejs");
+
+/////PASSPORT CONFIG
+app.use(require("express-session")({
+	secret:"book reader",
+	resave:false,
+	saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 
 app.use(indexRoutes);
 app.use(booksRoutes);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
 var express=require("express"),
-	router=express.Router();
+       User=require("../models/user"),
+   passport=require("passport"),
+	 router=express.Router();
 
 router.get("/",function(req,res){
     res.render("home");
+});
+
+router.get("/register",function(req,res){
+	res.render("register");
+});
+
+router.post("/register",function(req,res){
+	User.register(new User({username:req.body.username}),req.body.password,function(err,user){
+		if(err){
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req,res,function(){
+			res.redirect("/books")
+		})
+	})
 });	
 
 module.exports=router;
